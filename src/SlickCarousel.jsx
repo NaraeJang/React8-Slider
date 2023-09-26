0e04e95b2f8b2824fed796fbd24a61ea55dd9abd
NaraeJang <j.naaaarae@gmail.com>
2023-09-26 12:27:07 -0400

    react slick complete

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -5,38 +5,37 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import { shortList, list, longList } from "./data";
 import { FaQuoteRight } from "react-icons/fa";
+import { useState } from "react";
 
 const SlickCarousel = () => {
+  const [people, setPeople] = useState(longList);
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    pauseOnHover: true,
   };
 
   return (
     <section className="slick-container">
-      <h2> Single Item</h2>
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        {people.map((person) => {
+          const { id, image, name, title, quote } = person;
+
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h5 className="name">{name}</h5>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
       </Slider>
     </section>
   );
